22d789babea31333df9f47710eeced24d5a215b7
Shola Nur <shola.nur@gmail.com>
2023-02-14 12:57:09 -0800

    array task

---
+++ app.js
@@ -0,0 +1,31 @@
+let firstName = "Sharik";
+let last_name = "Vogue";
+
+let address = "Main Street";
+address = "First Street";
+
+console.log(firstName, last_name, address);
+
+let street = "65th St, New York";
+const country = "USA";
+let fullMailingAddress = street + " " + country;
+console.log(fullMailingAddress);
+
+let score1 = 5;
+let score2 = 4;
+let score3 = 3;
+
+let total = score1 + score2 + score3;
+let avarage = (score1 + score2 + score3) / 3;
+
+console.log(total);
+
+console.log(avarage);
+
+let plates = 20;
+let people = 7;
+
+let rem = plates % people;
+rem++;
+
+console.log(`There are ` + rem + ` ` + `plates available`);
---
+++ arrays.js
@@ -0,0 +1,5 @@
+let fruits = ["apple", "banana", "plumb", 8];
+let firstFruit = fruits[0];
+fruits[3] = "watermelon";
+console.log(firstFruit);
+console.log(fruits);
