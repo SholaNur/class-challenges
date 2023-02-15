6952ccad0d59a7a2b42ed32437e5af822fb16b58
Shola Nur <shola.nur@gmail.com>
2023-02-14 19:09:18 -0800

    added code

--- arrays.js
+++ arrays.js
@@ -3,3 +3,19 @@ let firstFruit = fruits[0];
 fruits[3] = "watermelon";
 console.log(firstFruit);
 console.log(fruits);
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+sum(4, 6);
+
+let arrNum = [1, 2, 4, 5, 6, 7, 8, 9, 20];
+let sumArr = 0;
+function takeAverage() {
+  for (let i = 0; i < arrNum.length; i++) {
+    sumArr += arrNum[i];
+  }
+  let avar = sumArr / arrNum.length;
+  return avar;
+}
+console.log(takeAverage());
